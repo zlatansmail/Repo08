9bfdd8349f21e935a51387e0df2e7d23e8721ec6
zlatansmail <zlatan.s@outlook.com>
2021-09-20 15:13:15 +0200

    ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+function convertStrToInt(str) {
+    str = parseInt(str);
+    return str;
+}
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+//Only change code above this line
+
+module.exports = convertStrToInt;
