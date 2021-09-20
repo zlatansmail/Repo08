f7221859e20299c3939f3ed77e7fc1c071e52f31
zlatansmail <zlatan.s@outlook.com>
2021-09-20 15:35:17 +0200

    errors corrected

---
+++ ex13/src/index.js
@@ -0,0 +1,9 @@
+//Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber) + 1) + minNumber;
+}
+
+console.log(randomRangeNumber(2, 100));
+
+//Only change code above this line
+module.exports = randomRangeNumber;
