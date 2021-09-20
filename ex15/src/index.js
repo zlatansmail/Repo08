df52a71cac533abefbe71b0696b1db07c98db97c
zlatansmail <zlatan.s@outlook.com>
2021-09-20 15:19:16 +0200

    ex15

---
+++ ex15/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function convertToInt(str) {
+    str = parseInt(str, 16);
+    return str;
+}
+
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertToInt;
