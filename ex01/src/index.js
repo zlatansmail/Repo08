bf7acee02047cbcc6a3473701fdbb3f5348e373c
zlatansmail <zlatan.s@outlook.com>
2021-09-20 12:06:07 +0200

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+//write code below this line
+
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+
+reusableFunction();
+//write code above this line
+
+module.exports = reusableFunction;
