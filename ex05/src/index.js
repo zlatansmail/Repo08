944665de8c9f227741cefda3c8b15a9fd0970106
zlatansmail <zlatan.s@outlook.com>
2021-09-20 13:29:57 +0200

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var dress = "little dress";
+
+function myDress() {
+    var dress = "ball gown"
+
+
+    //Only change code above this line
+    return dress;
+}
+// Only change code below this line
+myDress();
+//Only change code above this line
+console.log(dress);
+
+module.exports = myDress;
